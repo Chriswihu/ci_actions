d375a62bff0e30bb8b11b789e05d722aa5e7903a
Chriswihu <chris.wihu@gmail.com>
2023-02-02 09:31:36 +0100

    first

--- src/test/java/ci/MyFunctionalityTest.java
+++ src/test/java/ci/MyFunctionalityTest.java
@@ -47,6 +47,8 @@ public class MyFunctionalityTest {
         MyFunctionality instance = new MyFunctionality();
         assertEquals(10, instance.getTen());
 
+        System.out.println(instance.getTen());
+
     }
 
 }
